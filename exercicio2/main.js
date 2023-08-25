ebbadc27d30c4a93a74005ae76dd04ce56e782c8
Lucaspalomo <133269693+Lucaspalomo@users.noreply.github.com>
2023-08-24 21:59:57 -0300

    Algoritmo que calcula soma, subtração, divisão e multiplicação.

--- exercicio2/main.js
+++ exercicio2/main.js
@@ -1,8 +1,25 @@
-function Calcular(){
+function Calcular1(){
     // Variavel Local
     let numero1 = Number( document.getElementById ('numero1').value); //variaveis são não tipadas
     let numero2 = Number( document.getElementById ('numero2').value);
-    let numero3 = Number( document.getElementById ('numero3').value);
-    let resultado = (numero1 + numero2) * numero3;
-    document.getElementById('resultado').innerHTML = "O resultado é " + resultado;
+    let resultado = (numero1 + numero2);
+    document.getElementById('resultado').innerHTML = "O resultado da soma é " + resultado;
     }
+    function Calcular2(){
+    let numero1 = Number( document.getElementById ('numero1').value); //variaveis são não tipadas
+    let numero2 = Number( document.getElementById ('numero2').value);
+    let resultado = (numero1 - numero2);
+    document.getElementById('resultado').innerHTML = "O resultado da subtração é " + resultado;
+    }
+    function Calcular3(){
+    let numero1 = Number( document.getElementById ('numero1').value); //variaveis são não tipadas
+    let numero2 = Number( document.getElementById ('numero2').value);
+    let resultado = (numero1 * numero2);
+    document.getElementById('resultado').innerHTML = "O resultado da multiplicação é " + resultado;
+        }
+        function Calcular4(){
+            let numero1 = Number( document.getElementById ('numero1').value); //variaveis são não tipadas
+            let numero2 = Number( document.getElementById ('numero2').value);
+            let resultado = (numero1 / numero2);
+            document.getElementById('resultado').innerHTML = "O resultado da multiplicação é " + resultado;
+            }
