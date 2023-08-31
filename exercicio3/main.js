906521875322c3f894317a3ef47a9aa1aceffbac
Lucaspalomo <133269693+Lucaspalomo@users.noreply.github.com>
2023-08-31 20:17:38 -0300

    Lendo saldo de banco

--- exercicio3/main.js
+++ exercicio3/main.js
@@ -1,7 +1,7 @@
 function Calcular(){
     // Variavel Local
-    let numero = Number( document.getElementById ('numero1').value); //variaveis são não tipadas
-    let resultado = (numero ** 2);
-    document.getElementById('resultado').innerHTML = "O número elevado ao quadrado é: " + resultado;
+    let numero = Number( document.getElementById ('numero').value); //variaveis são não tipadas
+    let resultado = (numero);
+    document.getElementById('resultado').innerHTML = "O valor de entrada foi " + resultado;
     }
     
